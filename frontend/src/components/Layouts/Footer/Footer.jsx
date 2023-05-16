8676e89f52b1cae0b78abd8266efbf03fbdd7a1d
deepuku1234 <deepuhcverma1234@gmail.com>
2023-05-16 14:42:20 +0530

    update footer and header

--- frontend/src/components/Layouts/Footer/Footer.jsx
+++ frontend/src/components/Layouts/Footer/Footer.jsx
@@ -141,23 +141,23 @@ const Footer = () => {
             <div className="w-full sm:w-5/12 my-6 mx-5 sm:mx-0 flex flex-col sm:flex-row gap-2 sm:gap-0 justify-between">
               <div className="w-full sm:w-1/2">
                 <h2 className="text-primary-grey">Mail Us:</h2>
-                <p className="mt-2 leading-5">Flipkart Internet Private Limited,<br />
-                  Buildings Alyssa, Begonia &<br />
-                  Clove Embassy Tech Village,<br />
-                  Outer Ring Road, Devarabeesanahalli Village,<br />
-                  Bengaluru, 560103,<br />
-                  Karnataka, India
+                <p className="mt-2 leading-5">E-kaithel Internet Private Limited,<br />
+                  Meghnad saha bhawan &<br />
+                  nadia , kalyani,<br />
+                  University of kalyani,<br />
+                  west bengal, 741235,<br />
+                  west bengal, India
                 </p>
               </div>
 
               <div className="w-full sm:w-1/2">
                 <h2 className="text-primary-grey">Registered Office Address:</h2>
-                <p className="mt-2 leading-5">Flipkart Internet Private Limited,<br />
-                  Buildings Alyssa, Begonia &<br />
-                  Clove Embassy Tech Village,<br />
-                  Outer Ring Road, Devarabeesanahalli Village,<br />
-                  Bengaluru, 560103,<br />
-                  Karnataka, India <br />
+                <p className="mt-2 leading-5">E-kaithel Internet Private Limited,<br />
+                  Meghnad saha bhawan &<br />
+                  nadia , kalyani,<br />
+                  University of kalyani,<br />
+                  west bengal, 741235,<br />
+                  west bengal, India <br />
                   CIN : U51109KA2012PTC066107<br />
                   Telephone: <a className="text-primary-blue" href="tel:18002029898">1800 202 9898</a>
                 </p>
--- frontend/src/components/Layouts/Header/Header.jsx
+++ frontend/src/components/Layouts/Header/Header.jsx
@@ -23,11 +23,11 @@ const Header = () => {
     <header className="bg-success fixed top-0 py-2.5 w-full z-10">
 
       {/* <!-- navbar container --> */}
-      <div className="w-full sm:w-9/12 px-1 sm:px-4 m-auto flex justify-between items-center relative">
+      <div className="w-full  sm:w-9/12 px-1 sm:px-4 m-auto flex justify-between items-center relative">
 
         {/* <!-- logo & search container --> */}
         <div className="flex items-center flex-1">
-          <Link className="h-7 mr-1 sm:mr-4 text-white" to="/">
+          <Link className="h-7 mr-1 sm:mr-4 text-black" to="/">
           My ecommerce
           </Link>
 
@@ -42,7 +42,7 @@ const Header = () => {
             <Link to="/login" className="px-3 sm:px-9 py-0.5 text-primary-blue bg-white border font-medium rounded-sm cursor-pointer">Login</Link>
             :
             (
-              <span className="userDropDown flex items-center text-white font-medium gap-1 cursor-pointer" onClick={() => setTogglePrimaryDropDown(!togglePrimaryDropDown)}>{user.name && user.name.split(" ", 1)}
+              <span className="userDropDown flex items-center text-primary-blue font-medium gap-1 cursor-pointer" onClick={() => setTogglePrimaryDropDown(!togglePrimaryDropDown)}>{user.name && user.name.split(" ", 1)}
                 <span>{togglePrimaryDropDown ? <ExpandLessIcon sx={{ fontSize: "16px" }} /> : <ExpandMoreIcon sx={{ fontSize: "16px" }} />}</span>
               </span>
             )
@@ -50,13 +50,13 @@ const Header = () => {
 
           {togglePrimaryDropDown && <PrimaryDropDownMenu setTogglePrimaryDropDown={setTogglePrimaryDropDown} user={user} />}
 
-          <span className="moreDropDown hidden sm:flex items-center text-white font-medium gap-1 cursor-pointer" onClick={() => setToggleSecondaryDropDown(!toggleSecondaryDropDown)}>More
+          <span className="moreDropDown hidden sm:flex items-center text-black font-medium gap-1 cursor-pointer" onClick={() => setToggleSecondaryDropDown(!toggleSecondaryDropDown)}>More
             <span>{toggleSecondaryDropDown ? <ExpandLessIcon sx={{ fontSize: "16px" }} /> : <ExpandMoreIcon sx={{ fontSize: "16px" }} />}</span>
           </span>
 
           {toggleSecondaryDropDown && <SecondaryDropDownMenu />}
 
-          <Link to="/cart" className="flex items-center text-white font-medium gap-2 relative">
+          <Link to="/cart" className="flex items-center text-blue font-medium gap-2 relative">
             <span><ShoppingCartIcon /></span>
             {cartItems.length > 0 &&
               <div className="w-5 h-5 p-2 bg-red-500 text-xs rounded-full absolute -top-2 left-3 flex justify-center items-center border">
